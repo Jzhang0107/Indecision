11fdcf86095d2374ddc256f465c189bc4efdc728
Jason Zhang <zhang98@bu.edu>
2020-12-27 20:26:39 -0500

    Testing conditional rendering with React

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,11 +5,75 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
+
+var app = {
+    title: "Can't decide?",
+    subtitle: "we will help you choose!",
+    options: ['one', 'two']
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "Here are your options" : "No Options"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
+var user = {
+    name: "Jason",
+    age: 19,
+    location: 'New York City'
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name ? user.name : "Yukina"
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -3,7 +3,39 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
+
+var app = {
+    title: "Can't decide?",
+    subtitle: "we will help you choose!",
+    options: ['one', 'two']
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are your options":"No Options"}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: "Jason",
+    age: 19,
+    location: 'New York City'
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : "Yukina"}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        <p>Location: {user.location}</p>
+    </div>
+)
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
