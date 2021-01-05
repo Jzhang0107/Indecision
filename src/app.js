fb0200453e1ce1e3d6c38ac61db707c144a1a001
Jason Zhang <zhang98@bu.edu>
2021-01-04 21:50:03 -0500

    Installing react and react-dom using yarn

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
-// import './utils.js';
-import { square } from './utils.js';
-import isSenior, {isAdult, canDrink} from './person.js';
+// install -> import -> use
 
-console.log('Regina is cute!!!');
-console.log(square(5));
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log("Is an adult", isAdult(18));
-console.log("Can drink",canDrink(20));
-console.log("Is senior",isSenior(65));
