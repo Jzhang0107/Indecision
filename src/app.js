6b7be9b779559a6ed7a3b33d346e7f0b92ad520c
Jason Zhang <zhang98@bu.edu>
2021-01-05 21:23:43 -0500

    Added css file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.css';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
