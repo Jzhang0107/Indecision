251e4a023900a65ee5af3831ce14a4a26bb7a546
Jason Zhang <zhang98@bu.edu>
2021-01-07 00:42:07 -0500

    Added mobile compatibility css and a favicon

--- src/components/Options.js
+++ src/components/Options.js
@@ -8,7 +8,6 @@ const Options = (props) =>
             <h3 className='widget-header__title'>Your options:</h3>
             <button className='button button--remove' onClick={props.removeAllOptions}>Remove all options</button>
         </div>
-        <br />
         {props.options.length <= 0 && <p className='widget__message'>There are no current options</p>}
         {props.options.map((item, index) => 
             (
